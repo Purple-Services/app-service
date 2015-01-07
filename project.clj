665c64de75fc9a0e291a78d0edd075933782bafa
Christopher Elwell <elwell.christopher@gmail.com>
2015-01-07 16:23:43 -0500

    round 2 of trying to embed facebook-graph lib

--- project.clj
+++ project.clj
@@ -7,11 +7,13 @@
                  [c3p0/c3p0 "0.9.1.2"] ;; db connection pooling
                  [org.clojure/java.jdbc "0.2.3"]
                  [mysql/mysql-connector-java "5.1.18"]
-                 [cheshire "4.0.3"] ;; json and related utilities
+                 [cheshire "5.4.0"] ;; json and related utilities
                  [clj-http "1.0.0"]
                  [crypto-password "0.1.3"]
                  [org.clojars.freeagent/clj-facebook-graph "0.4.0"]
-                 [gapi "1.0.1"]]
+                 ;; [com.draines/postal "1.11.3"]
+                 [gapi "1.0.1"]
+                 ]
   ;; :pedantic? :warn
   :plugins [[lein-ring "0.8.13"]
             [lein-beanstalk "0.2.7"]]
@@ -21,6 +23,6 @@
          :reload-paths ["src"]}
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                                   [ring-mock "0.1.5"]]}}
-  :aot [clj-facebook-graph.FacebookGraphException]
+  ;; :aot [clj-facebook-graph.FacebookGraphException]
   :aws {:beanstalk {:s3-bucket "leinbeanstalkpurple"
                     :region "us-west-2"}})
