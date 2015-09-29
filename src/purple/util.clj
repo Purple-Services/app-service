b7c004d2fb486812789c29fe1cb5e3a30b6fbb7a
Christopher Elwell <elwell.christopher@gmail.com>
2015-09-29 11:26:09 -0700

    remove chris from feedback emails

--- src/purple/util.clj
+++ src/purple/util.clj
@@ -164,18 +164,16 @@
   (let [user (when user_id
                ((resolve 'purple.users/get-user-by-id) (conn) user_id))]
     (only-prod
-     (send-email (conj {:to "chris@purpledelivery.com"
-                        :cc ["joe@purpledelivery.com"
-                             "bruno@purpledelivery.com"]
-                        :subject "Purple Feedback Form Response"
-                        :body (if user
-                                (str "From User ID: " user_id "\n\n"
-                                     "Name: " (:name user) "\n\n"
-                                     "Email: " (:email user) "\n\n"
-                                     text)
-                                text)}
-                       (when user
-                         [:reply-to (:email user)]))))))
+     (send-email {:to "chris@purpledelivery.com"
+                  :cc ["joe@purpledelivery.com"
+                       "bruno@purpledelivery.com"]
+                  :subject "Purple Feedback Form Response"
+                  :body (if user
+                          (str "From User ID: " user_id "\n\n"
+                               "Name: " (:name user) "\n\n"
+                               "Email: " (:email user) "\n\n"
+                               text)
+                          text)}))))
 
 ;; Amazon SNS (Push Notifications)
 (! (do
