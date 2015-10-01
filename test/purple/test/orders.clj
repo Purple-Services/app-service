8cfeef1a4053692a04489e66f299a7276ab48b9a
James Borden <jmborden@gmail.com>
2015-10-01 12:48:36 -0500

    added tests for modifying zones in dashboard and courier zone assignment

--- test/purple/test/orders.clj
+++ test/purple/test/orders.clj
@@ -1,6 +1,6 @@
 (ns purple.test.orders
   (:require [purple.orders :as orders]
-            [purple.db :refer [!select conn]]
+            [purple.db :refer [!select conn !update]]
             [purple.dispatch :as dispatch]
             [purple.test.db :refer [db-config]]
             [clojure.test :refer [use-fixtures deftest is test-ns testing]]))
@@ -45,3 +45,52 @@
   "Add an order to the database"
   [order]
   (orders/add db-config (:user_id order) order))
+
+(defn unassigned-orders
+  "Can the courier only see unassigned orders in their assigned zone?"
+  []
+  (let [zone-id 3
+        zone-zip (-> (filter #(= (:id %) 3) @dispatch/zones)
+                     first
+                     :zip_codes
+                     first)
+        order     (assoc (test-order) :address_zip zone-zip)
+        courier-id "lGYvXf9qcRdJHzhAAIbH"]
+    ;; change Test Courier 1's zones to just 1
+    (!update db-config "couriers" {:zones "1"} {:id courier-id})
+    ;; test that the zone id is correct
+    (is (= zone-id (:id (dispatch/get-zone-by-zip-code zone-zip))))
+    ;; add an order to zone 3
+    (add-order order)
+    ;; Test Courier 1 should not be able to see the unassigned order
+    ;; this assumes there are no unassigned orders in the couriers zone!
+    (is (= 0 (count (filter #(= (:status %) "unassigned")
+                            (orders/get-by-courier
+                             db-config
+                             courier-id)))))
+    ;; change Test Courier 1's zones to include zone 3
+    (!update db-config "couriers" {:zones "1,3"} {:id courier-id})
+    ;; Test Courier 1 should be able to see it
+    (is (= 1 (count (filter #(= (:status %) "unassigned")
+                            (orders/get-by-courier
+                             db-config
+                             courier-id)))))
+    ;; Change Test Courier 1's zones back to just "1"
+    (!update db-config "couriers" {:zones "1"} {:id courier-id})
+    ;; test courier 1 should no longer be able to see it
+    (is (= 0 (count (filter #(= (:status %) "unassigned")
+                            (orders/get-by-courier
+                             db-config
+                             courier-id)))))
+    ;; cancel the order. Note: this assumes there are no other
+    ;; unassigned orders!
+    (let [order (first (!select db-config
+                                "orders"
+                                [:id :user_id]
+                                {:status "unassigned"}))]
+      (orders/cancel db-config (:user_id order) (:id order)
+                     :notify-customer false
+                     :suppress-user-details true))))
+
+(deftest test-dispatch
+  (unassigned-orders))
