9328d10a621562f1f5c70b1bd208d747f6180c5e
Christopher Elwell <elwell.christopher@gmail.com>
2016-01-19 14:33:28 -0800

    add Rana to text messages

--- src/purple/dispatch.clj
+++ src/purple/dispatch.clj
@@ -349,8 +349,9 @@
     (only-prod (run!
                 #(send-sms % "There are orders, but no available couriers.")
                 (concat [] ;; put your number in here when dev'ing
-                        (only-prod ["3103109961"     ;; Joe
-                                    "7143154380"     ;; Gustavo
+                        (only-prod ["3103109961" ;; Joe
+                                    "7143154380" ;; Gustavo
+                                    "3234592100" ;; Rana
                                     ]))))
     (reset! last-orphan-warning (quot (System/currentTimeMillis) 1000))))
 
