a0a5d24c93193725fef7c1a6aaeed73eeae75010
James <jmborden@gmail.com>
2016-04-27 10:50:08 -0500

    updated documentation in test/app/test/db.clj

--- test/app/test/db.clj
+++ test/app/test/db.clj
@@ -41,8 +41,7 @@
   (let [sql-lines (->
                    (slurp filename) ; read in the sql file
                    (clojure.string/replace #"--.*\n" "") ; ignore sql comments
-                   (clojure.string/split #";\n") ; sepereate chunks into
-                                                 ; statements
+                   (clojure.string/split #";\n") ; separeate into statements
                    )]
     (->> sql-lines
          (map #(clojure.string/replace % #"\n" ""))
@@ -64,7 +63,7 @@
 
 (defn clear-and-populate-test-database
   []
-   ;; start with a clean ebdb_test database
+  ;; start with a clean ebdb_test database
   (clear-test-database)
   ;; populate the tables
   (create-tables-and-populate-database ebdb-test-config))
@@ -74,13 +73,25 @@
   (db/set-pooled-db! ebdb-test-config)
   (clear-and-populate-test-database))
 
-;; THIS FIXTURE REQUIRES A LOCAL MySQL DATABASE THAT HAS GIVEN PROPER
+;; THIS FIXTURE REQUIRES A LOCAL MySQL DATABASE THAT HAS BEEN GIVEN PROPER
 ;; PERMISSIONS TO purplemaster FOR ebdb_test, OTHERWISE TESTS WILL FAIL!
 ;;
-;; YOU SHOULD RUN THE FOLLOWING SCRIPT DESCRIBED IN README.md FIRST!
-;; $ lein exec -p resources/scripts/setupdb.clj root_password=<mysql_root_pwd>
+;; see: database/ebdb_setup.sql for proper permissions
 ;;
-;; see: resources/database/ebdb_setup.sql for proper permissions
+;;
+;; IF YOU DO NOT HAVE A LOCAL DATABASE SETUP,
+;; YOU SHOULD RUN THE FOLLOWING SCRIPT DESCRIBED IN README.md FIRST!
+;;
+;; $ lein exec -p scripts/setupdb.clj root_password=<mysql_root_pwd>
+;;
+;; NOTE: THIS WILL drop all tables, if you have an existing local database,
+;; see database/eddb_setup.sql for creating the proper permissions for
+;; ebdb_test.*
+;;
+;; You will also need to populate the local database with data, see
+;; scripts/retrieve_db
+
+
 (defn database-fixture
   "Remove all test data from the database"
   [test]
