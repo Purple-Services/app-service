948db83d80cc25a59ff0f5deaef23b0d58888237
Christopher Elwell <elwell.christopher@gmail.com>
2016-07-22 17:09:28 -0700

    error message when no gallons inputted

--- src/app/fleet.clj
+++ src/app/fleet.clj
@@ -18,17 +18,20 @@
 
 (defn add-delivery
   [db-conn account-id courier-id vin license-plate gallons gas-type is-top-tier]
-  (!insert db-conn
-           "fleet_deliveries"
-           {:id (rand-str-alpha-num 20)
-            :courier_id courier-id
-            :account_id account-id
-            :vin vin
-            :license_plate license-plate
-            :gallons gallons
-            :gas_type gas-type
-            :is_top_tier is-top-tier
-            :gas_price (-> (get-account-by-id db-conn account-id)
-                           :address_zip
-                           get-fuel-prices
-                           (get (keyword gas-type)))}))
+  (cond
+    (= 0 gallons) {:sucess false :message "You must enter the number of gallons delivered."}
+    :else (!insert db-conn
+                   "fleet_deliveries"
+                   {:id (rand-str-alpha-num 20)
+                    :courier_id courier-id
+                    :account_id account-id
+                    :vin (s/upper-case vin)
+                    :license_plate (s/upper-case license-plate)
+                    :gallons gallons
+                    :gas_type gas-type
+                    :is_top_tier (or is-top-tier false)
+                    :gas_price (-> (get-account-by-id db-conn account-id)
+                                   :address_zip
+                                   get-fuel-prices
+                                   (get (keyword gas-type)))})))
+
