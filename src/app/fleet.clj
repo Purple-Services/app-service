facc1b0fe17c0e2161cf6b1bd6a4f4c08a624c8c
Christopher Elwell <elwell.christopher@gmail.com>
2016-08-02 12:32:28 -0700

    check for vin or license plate presence

--- src/app/fleet.clj
+++ src/app/fleet.clj
@@ -1,7 +1,7 @@
 (ns app.fleet
   (:require [common.config :as config]
             [common.db :refer [!select !insert !update mysql-escape-str]]
-            [common.util :refer [in? rand-str-alpha-num]]
+            [common.util :refer [rand-str-alpha-num]]
             [common.zones :refer [get-fuel-prices]]
             [clojure.string :as s]))
 
@@ -17,21 +17,28 @@
      :default_account_id (:id (first accounts))}))
 
 (defn add-delivery
+  "Report a delivery of fuel for a fleet account."
   [db-conn account-id courier-id vin license-plate gallons gas-type is-top-tier]
   (cond
-    (= 0 gallons) {:sucess false :message "You must enter the number of gallons delivered."}
-    :else (!insert db-conn
-                   "fleet_deliveries"
-                   {:id (rand-str-alpha-num 20)
-                    :courier_id courier-id
-                    :account_id account-id
-                    :vin (s/upper-case vin)
-                    :license_plate (s/upper-case license-plate)
-                    :gallons gallons
-                    :gas_type gas-type
-                    :is_top_tier (or is-top-tier false)
-                    :gas_price (-> (get-account-by-id db-conn account-id)
-                                   :address_zip
-                                   get-fuel-prices
-                                   (get (keyword gas-type)))})))
+    (and (s/blank? vin) (s/blank? license-plate))
+    {:sucess false :message "You must enter a VIN or a License Plate."}
+    
+    (= 0 gallons)
+    {:sucess false :message "You must enter the number of gallons delivered."}
+    
+    :else
+    (!insert db-conn
+             "fleet_deliveries"
+             {:id (rand-str-alpha-num 20)
+              :courier_id courier-id
+              :account_id account-id
+              :vin (s/upper-case vin)
+              :license_plate (s/upper-case license-plate)
+              :gallons gallons
+              :gas_type gas-type
+              :is_top_tier (or is-top-tier false)
+              :gas_price (-> (get-account-by-id db-conn account-id)
+                             :address_zip
+                             get-fuel-prices
+                             (get (keyword gas-type)))})))
 
