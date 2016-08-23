3bd73225206e84c940d62eac18c1796778909216
Christopher Elwell <elwell.christopher@gmail.com>
2016-08-23 16:36:49 -0700

    trying another travis file round 22

--- database/ebdb_setup.sql
+++ database/ebdb_setup.sql
@@ -2,5 +2,3 @@ DROP DATABASE if exists ebdb;
 DROP DATABASE if exists ebdb_test;
 create database if not exists ebdb;
 create database if not exists ebdb_test;
-grant all on ebdb.* to 'purplemaster'@'localhost'  identified by 'localpurpledevelopment2015';
-grant all on ebdb_test.* to 'purplemaster'@'localhost' identified by 'localpurpledevelopment2015';
