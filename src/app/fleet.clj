78988f65ad2b842dd39865f6ad8930083c839cc7
Christopher Elwell <elwell.christopher@gmail.com>
2016-11-09 15:43:25 -0800

    service_fee and total_price in fleet deliveries

--- src/app/fleet.clj
+++ src/app/fleet.clj
@@ -18,31 +18,37 @@
 
 (defn add-delivery
   "Report a delivery of fuel for a fleet account."
-  [db-conn fleet-location-id courier-id vin license-plate gallons gas-type is-top-tier]
+  [db-conn fleet-location-id courier-id vin license-plate gallons-str gas-type is-top-tier]
   (cond
     (and (s/blank? vin) (s/blank? license-plate))
     {:sucess false :message "You must enter a VIN or a License Plate."}
     
-    (= 0 gallons)
+    (= 0 (Double. gallons-str))
     {:sucess false :message "You must enter the number of gallons delivered."}
     
     :else
-    (!insert db-conn
-             "fleet_deliveries"
-             {:id (rand-str-alpha-num 20)
-              :courier_id courier-id
-              :fleet_location_id fleet-location-id
-              :vin (s/upper-case vin)
-              :license_plate (s/upper-case license-plate)
-              :gallons gallons
-              :gas_type gas-type
-              :is_top_tier (or is-top-tier false)
-              ;; for reference, record gas price at this point in space-time
-              :gas_price (-> (get-fleet-location-by-id db-conn fleet-location-id)
-                             :address_zip
-                             (#(get-zip-def db-conn %))
-                             :gas-price
-                             (get gas-type))})))
+    (let [gas-price (-> (get-fleet-location-by-id db-conn fleet-location-id)
+                        :address_zip
+                        (#(get-zip-def db-conn %))
+                        :gas-price
+                        (get gas-type))
+          service-fee 0
+          gallons (Double. gallons-str)]
+      (!insert db-conn
+               "fleet_deliveries"
+               {:id (rand-str-alpha-num 20)
+                :courier_id courier-id
+                :fleet_location_id fleet-location-id
+                :vin (s/upper-case vin)
+                :license_plate (s/upper-case license-plate)
+                :gallons gallons
+                :gas_type gas-type
+                :is_top_tier (or is-top-tier false)
+                :gas_price gas-price
+                :service_fee service-fee
+                :total_price ((comp (partial max 0) int #(Math/ceil %))
+                              (+ (* gas-price gallons)
+                                 service-fee))}))))
 
 (defn add-deliveries
   [db-conn
